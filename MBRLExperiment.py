5cbb84728de635e4a4513b8ec29ed197edbfb11d
Emmanouil Zagoritis <zagoritisemmanuel@gmail.com>
2025-05-08 00:53:04 +0200

    Modified dyna plot, run_repetitions()

--- MBRLExperiment.py
+++ MBRLExperiment.py
@@ -17,39 +17,40 @@ def experiment():
     n_repetitions = 20
     gamma = 1.0
     learning_rate = 0.2
-    epsilon=0.1
+    epsilon = 0.1
     
-    wind_proportions=[0.9,1.0]
-    n_planning_updates = [1,3,5] 
-    
-    ''' Dyna '''
-    mean_returns = run_repetition("dyna", n_repetitions, n_timesteps, eval_interval, n_planning_updates, learning_rate, epsilon, gamma)
-    plot = LearningCurvePlot(title='Dyna')
-    plot.add_curve(np.arange(0, n_timesteps, eval_interval), mean_returns, label='Dyna')
-    plot.set_ylim(-10, 0)
-    plot.add_hline(-1, 'Optimal')
-    plot.save('dyna.png')
-    
-    ''' Prioritized Sweeping '''
-    ###
+    wind_proportions = [0.9,1.0]
+    n_planning_updatess = [1,3,5]
 
-def run_repetition(agent_type, n_repetitions, n_timesteps, eval_interval, n_planning_updates, learning_rate=0.1, epsilon=0.1, gamma=1.0):
+    for wind_proportion in wind_proportions:
+        plot = LearningCurvePlot(title='Dyna')
+        for n_planning_updates in n_planning_updatess:
+            mean_returns = run_repetition("dyna", n_repetitions, n_timesteps, eval_interval, wind_proportion, n_planning_updates, learning_rate, epsilon, gamma)
+            smooth_plot = smooth(mean_returns, window=5)
+            plot.add_curve(np.arange(0, n_timesteps, eval_interval), smooth_plot, label=f'Dyna{n_planning_updates} plans')
+        plot.set_ylim(-2000, 100)
+        plot.save(f'Dyna{wind_proportion} wind.png')
+
+def run_repetition(agent_type, n_repetitions, n_timesteps, eval_interval, wind_proportion, n_planning_updates, learning_rate=0.1, epsilon=0.1, gamma=1.0):
     total_returns = []
-    env = WindyGridworld()
+    env = WindyGridworld(wind_proportion=wind_proportion)
     for _ in range(n_repetitions):
-        s = env.reset()
         if agent_type == 'dyna':
             agent = DynaAgent(n_states=env.n_states, n_actions=env.n_actions, learning_rate=learning_rate, gamma=gamma)
         elif agent_type == 'prioritizedsweeping':
             agent = PrioritizedSweepingAgent(n_states=env.n_states, n_actions=env.n_actions, learning_rate=learning_rate, gamma=gamma)
 
-        for _ in range(n_timesteps):
+        s = env.reset()
+        eval_returns = []
+        for i in range(n_timesteps):
             a = agent.select_action(s, epsilon)
             s_next, r, done = env.step(a)
             agent.update(s=s, a=a, r=r, done=done, s_next=s_next, n_planning_updates=n_planning_updates)
 
-            if eval_interval == 250:
-                 total_returns.append(agent.evaluate(env))
+            if i % eval_interval == 0:
+                eval_returns.append(agent.evaluate(env))
+                print(i)
+        total_returns.append(eval_returns)
     return np.mean(total_returns, axis=0)
 
 if __name__ == '__main__':
